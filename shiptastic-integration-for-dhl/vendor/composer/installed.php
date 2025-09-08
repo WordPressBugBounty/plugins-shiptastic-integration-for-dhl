6bc6215235aed23b1bf4e30fb4217d624b37699c
Sean Murphy <sean@iamseanmurphy.com>
2025-09-08 09:20:16 +0000

    4.1.3

--- shiptastic-integration-for-dhl/vendor/composer/installed.php
+++ shiptastic-integration-for-dhl/vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'vendidero/shiptastic-integration-for-dhl',
-        'pretty_version' => '4.1.2',
-        'version' => '4.1.2.0',
+        'pretty_version' => '4.1.3',
+        'version' => '4.1.3.0',
         'reference' => null,
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -41,8 +41,8 @@
             ),
         ),
         'vendidero/shiptastic-integration-for-dhl' => array(
-            'pretty_version' => '4.1.2',
-            'version' => '4.1.2.0',
+            'pretty_version' => '4.1.3',
+            'version' => '4.1.3.0',
             'reference' => null,
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
